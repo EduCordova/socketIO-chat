01ea7b7ad8392f389d25f2ec774e4489b4327c02
Doo <kde1690@gmail.com>
2018-03-10 20:13:28 -0500

    first commit

---
+++ client/main.js
@@ -0,0 +1,33 @@
+var socket = io.connect('http://192.168.1.3:6677',{'forceNew':true});
+
+socket.on('messages',function(data){
+    console.log(data)
+    render(data)
+})
+
+function render(data){
+    var html = data.map(function(i,key){
+        return (`
+        <div class="message">
+            <strong>${i.nickname}</strong> dice:
+            <p>${i.text}</p>
+        </div>`)
+    }).join(' ');
+
+    var div_msgs = document.getElementById('messages')
+    div_msgs.innerHTML = html
+    div_msgs.scrollTop  = div_msgs.scrollHeight
+}
+
+function addMessage(e){
+    var message = {
+        id:Math.floor((Math.random() * 1000) + 1),
+        nickname:document.getElementById('nickname').value,
+        text:document.getElementById('text').value
+        
+    }
+    document.getElementById('nickname').style.display = 'none';
+    socket.emit('add-message',message);
+    document.getElementById('text').value =" "
+    return false
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+
+//var port = process.env.PORT || 3001
+
+app.use(express.static('client'));
+
+app.get('/',(req,res)=>{
+    res.status(200).send("hola mundo desde una ruta")
+})
+
+
+var messages =[
+    {
+        id:1,
+        text:"Bienvenido a este chat privado",
+        nickname:'bot-Educordova.es'
+    }
+]
+
+io.on('connection',function(socket){
+    console.log(`El nodo con ip: ${socket.handshake.address} se a conectado`)
+    socket.emit('messages',messages)
+
+    socket.on('add-message',function(data){
+        messages.push(data)
+        io.sockets.emit('messages',messages)
+    })
+})
+
+server.listen(6677,()=>{
+    console.log('El servidor esta corriendo... en 6677')
+})
